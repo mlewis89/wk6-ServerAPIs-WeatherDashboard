e16fb012717c0639cf1fab03b05e3eff431b0569
Mark Lewis <mlewis89@gmail.com>
2024-01-16 09:48:56 +1100

    api calls functioning code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,6 +4,7 @@ SeachDataEL = $('#search-text');
 
 //GLOBAL: VARIABLES
 storageKey = "wk6-WeatherDashboard";
+
 APIkey = "206a083510fdb299f5bde1e92a72e4f7";
 var currentWeather = {};
 
@@ -12,7 +13,7 @@ SearchButtonEL.on('click',function()
 {
     var SearchText = SeachDataEL.val();
     updateLocalStorage(SearchText);
-    currentWeather = getWeatherData(SearchText);
+    getWeatherData_FromCityName(SearchText);
     renderPage();
 });
 
@@ -31,36 +32,71 @@ function updateLocalStorage(newVal)
 //clear local storage
 
 //get location lat and long - openweatrher map direct geocoding
-function geocodelocation(str)
+function getWeatherData_FromCityName(city)
 {   
-    var url = 
-    var geocode = apiGet(ulr);
-    var obj = {
-        lat: "",
-        long: "",
-    };
-
-    return obj;
-}
-
-function apiGet(url)
-{
-    fetch(url).then(function (response){
-        if(response.ok){
-            response.json().then(function (data){
-                return data;
-            });
-        }else{
-            return ('Error: ' + response.statusText);
-        }
+    var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=1&appid='+APIkey;
+    //var geocode = apiGet(url);
+    fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+            console.log(data);
+          getWeatherData_FromLatLong(data[0].lat,data[0].lon,city);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
     })
+    .catch(function (error) {
+      alert('Unable to connect to openmweathermap.org');
+    });
 }
 
 //API Get Location weather
-function getWeatherData(search)
+function getWeatherData_FromLatLong(lat,long)
 {
-    return;
+    var apiUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+long+'&appid='+APIkey
+    
+    fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayWeather(data);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to openmweathermap.org');
+    });
 }
+    
+function displayWeather(weatherData)
+{
+    console.log(data);
+    console.log(str);
+
+}
+
+function apiGet(apiUrl)
+{
+    fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          return data;
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to GitHub');
+    });
+}
+
+
 
 function API_GET()
 {}
